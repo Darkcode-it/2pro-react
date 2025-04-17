48b450d82f68d95e21d7994c86e92f5e8688c695
DarkCode <darkcodeit@gmail.com>
2025-04-17 14:24:50 +0330

    Update index.html to support RTL layout and change language to Persian; remove outdated CSS and JS files; add Onediv component to Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,15 @@ import There from "../components/searchbox/There";
 import Footer from "../components/footer/Footer";
 import Menu from "../components/menu/Menu";
 import data from '../components/menu/Menu.json';
-
+import onediv from "../components/census/Onediv"
+import Onediv from "../components/census/Onediv";
 
 const Home = () => {
     return (
         <div>
             <Menu data={data} />
             <Header />
+            <Onediv/>
             <Articles />
             <Twodiv />
             <There />
